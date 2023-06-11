e828e868d5d811eac1fae7a1f97e6c3f206d2898
AziSudarya1 <115763620+AziSudarya1@users.noreply.github.com>
2023-06-11 11:45:43 +0700

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const about = document.getElementById('about')
+const menu = document.getElementById('menu')
+const contact = document.getElementById('contact')
+function scrolltoabout (){
+    about.scrollIntoView()
+}
+
+function scrolltomenu (){
+    menu.scrollIntoView()
+}
+function scrolltocontact (){
+    contact.scrollIntoView()
+}
